d072536aac7c42fd5cea7a6d23a0413af14f4ee8
tlp19 <58601202+tlp19@users.noreply.github.com>
2021-03-14 18:13:33 +0000

    updated script

--- update_and_start_server.sh
+++ update_and_start_server.sh
@@ -2,10 +2,15 @@
 set -e
 
 # Pull the latest version of the repository
+echo "## Getting latest version from GitHub"
 git pull origin master
 
 # Compile the server
+echo ""
+echo "## Compiling tcp_server.cpp"
 g++ tcp_server.cpp -o tcp_server
 
 # Start the server on port 7000 (must be set as inbound rule on AWS/Azure)
+echo ""
+echo "Starting server on port 7000"
 ./tcp_server 7000
