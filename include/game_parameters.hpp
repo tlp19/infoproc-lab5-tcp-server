8126fdc8a6eb6d6ef7e9a5c01c6eb6cf94a35d05
tlp19 <58601202+tlp19@users.noreply.github.com>
2021-03-16 16:45:29 +0000

    update

--- include/game_parameters.hpp
+++ include/game_parameters.hpp
@@ -5,13 +5,13 @@
 
 namespace Parameters {
 
-   const int backlog=1000;  // Number of pending connections before clients are refused.
-                                 // I.e. maximum number of player.
-                                 // Maximum: 1000
+   const int backlog=10;   // Number of pending connections before clients are refused.
+                           // I.e. maximum number of player.
+                           // Maximum: 1000
 
-   const int angle_range=100;   // Range that X and Y angles will be generated in.
-                                       // Angles are centered around zero, so actual range is [-angle_range/2 ; angle_range/2]
-                                       // Recommanded: range value 100 (eq. to [-50 ; 50])
-                                       // Maximum: 510 (eq. to [-255 ; 255])
+   const int angle_range=100;    // Range that X and Y angles will be generated in.
+                                 // Angles are centered around zero, so actual range is [-angle_range/2 ; angle_range/2]
+                                 // Recommanded: range value 100 (eq. to [-50 ; 50])
+                                 // Maximum: 510 (eq. to [-255 ; 255])
 }
 #endif
