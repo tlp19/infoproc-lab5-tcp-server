ac8dfba18946944cba13782b157d95cfac7a58e1
tlp19 <58601202+tlp19@users.noreply.github.com>
2021-03-16 03:15:24 +0000

    working on shell scripts

--- update_and_run_client.sh
+++ update_and_run_client.sh
@@ -11,11 +11,11 @@ git pull origin master
 echo ""
 echo "## Compiling src/tcp_client.cpp"
 echo ""
-g++ src/tcp_client.cpp -o bin/tcp_client
-echo "log: client compiled as ./bin/tcp_client"
+g++ src/tcp_client.cpp -o src/tcp_client
+echo "log: client compiled as ./src/tcp_client"
 
 # Connect to the server adsress and port with client
 echo ""
 echo "## Connecting to server at address $1 on port 7000"
 echo ""
-./bin/tcp_client $1:7000
+./src/tcp_client $1:7000
--- update_and_start_server.sh
+++ update_and_start_server.sh
@@ -11,11 +11,11 @@ git pull origin master
 echo ""
 echo "## Compiling src/tcp_server.cpp"
 echo ""
-g++ src/tcp_server.cpp -o bin/tcp_server
-echo "log: server compiled as ./bin/tcp_server"
+g++ src/tcp_server.cpp -o src/tcp_server
+echo "log: server compiled as ./src/tcp_server"
 
 # Start the server on port 7000 (must be set as inbound rule on AWS/Azure)
 echo ""
 echo "## Starting server on port 7000"
 echo ""
-./bin/tcp_server 7000
+./src/tcp_server 7000
